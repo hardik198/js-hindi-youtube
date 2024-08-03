83350bf1c5d51940cdad70c495716f7250e9ad8f
hardik198 <panchalhardik183@gmail.com>
2024-08-04 00:14:14 +0530

    Add files via upload

---
+++ 01_datatypes.js
@@ -0,0 +1,26 @@
+// alert(3 + 3) //we are using nodejs,not browser
+
+// console.log(3 + 3)
+// console.log("hardik")
+
+// let name="hardik"
+// let age=19
+// let isLoggedIn=false
+
+//number ==> 2 to power 53
+//bignit
+//string ==> "hardik"
+//boolean ==> true/false
+//null ==> standalone value (emply value)
+// undefined ==>
+// symbol ==> uniqueness 
+
+//object 
+
+console.log(typeof 23);                    //number
+console.log(typeof "hardik");             //string
+console.log(typeof null);                //object
+console.log(typeof undefined);           //undefined
+
+
+
