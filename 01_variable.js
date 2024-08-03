5aa40d8145d9e69d154b2b6ea113dd3505ea575a
hardik198 <panchalhardik183@gmail.com>
2024-08-04 00:14:58 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="hardikpanchal183@gmail.com"
+var accountPassword="12345"
+accountCity="palanpur"
+let accountstate;
+
+
+// const are not change,var are used to all places chanegs and scope problems so var are never used ,let is used to one places changes so it is always used;// 
+
+accountEmail="panchalhardik42@gmail.com"
+accountPassword=3445666;
+accountCity="patan";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
+
