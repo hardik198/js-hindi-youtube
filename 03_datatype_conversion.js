ba88b206376181a9d47f31651cce0463c9bcd209
hardik198 <panchalhardik183@gmail.com>
2024-08-04 00:40:29 +0530

    Add files via upload

---
+++ 03_datatype_conversion.js
@@ -0,0 +1,25 @@
+// let scope = "33"
+
+// console.log(typeof scope)
+
+// let valueInNumber = Number(scope)
+
+// console.log(typeof valueInNumber)
+
+// "33" string convert to number ==>33
+//"33abc" string conver to ==> NaN
+// true convert to ==> 1
+//false conver to ==> 0
+// "" convert to ==> false
+// "hardik" convert to ==> treu
+
+// let isLoggedIn = "hardik"
+// let booleansIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleansIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+
+
