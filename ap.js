5156de7d8a83afce0eff96c56148fcddfc5c6df8
hardik198 <panchalhardik183@gmail.com>
2024-08-03 23:11:10 +0530

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,86 @@
+// let answer=0;
+// answer=1+2+3+4+5+6;
+// console.log(answer);
+// let answer=0;
+// for(let i=0;i<=5;i++)
+// {
+//     answer=answer+i;
+// }
+// console.log(answer)
+
+// let per1="hardik";
+// let per2="meet";
+// let per3="hitu";
+// const perarray=["hardik","meet","hitu"];
+// console.log(perarray[2]);
+
+// let ages=[21,22,23,24,25,26,27,28,29,30];
+// let nop=ages.length;
+
+// for(let i=0;i<nop;i++)
+// {
+//     if(ages[i]%2==0)
+//     {
+//         console.log(ages[i]);
+//     }
+// }
+
+
+// ages.forEach((e)=>{
+//     e%2==0 ? console.log(e) : 
+// })
+// const allusers=[{
+//                    fname:"hardik",
+//                    gender:"male",
+//                    metadata :{
+//                      age:18,
+//                      address:"bhildi"
+
+//                    },
+// },
+// {
+//     fname:"jay",
+//     gender:"male"
+// },
+// {
+//     fname:"kasu",
+//     gender:"female"
+// }]
+
+// for(let i=0;i<allusers.length;i++)
+// {
+//      if(allusers[i]["gender"]=="male")
+//      {
+//     console.log(allusers[i]["fname"]);
+//      }
+// }
+// function sum(a,b){
+//     return a+b;
+// }
+// const value=sum(1,2);
+// console.log(value);
+
+
+// 
+
+
+// function greetalian()
+// {
+//     console.log("hello alian");
+// }
+//  setInterval(greetalian,3*1000);
+// function sum(a,b){
+//     let r=a+b;
+//     return r;
+// }
+// let x=sum(2,3);
+// console.log(x);
+// const user={
+//     name:"hardik",
+//     age:21,
+//     address :{
+//         hnumber:"55",
+//         street: "11"
+//     }
+// }
+// console.log(user["address"]["hnumber"]);
