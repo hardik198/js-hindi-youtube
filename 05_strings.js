ddbb4092b0edd2c2586644d1bbe3fe5c23c1af05
hardik198 <panchalhardik183@gmail.com>
2024-08-07 01:44:43 +0530

    Add files via upload

---
+++ 05_strings.js
@@ -0,0 +1,62 @@
+const name = " I am hardik"
+const repocount=50
+
+// console.log(name+repocount + " hello world");   <==this method is normal
+
+// console.log(`hello world ${name} this is programming ${repocount}`);
+
+//string declared karne ka best method ==>
+
+// const gamename = new String('hardikp');
+
+//  console.log(gamename.__proto__);
+
+// 2)
+// const gamename = new String('hardikp');
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));<=== string mai index se character knowing mate
+
+// console.log(gamename.indexOf('h')); <=== character se index knowing mate
+
+
+
+// substring  string  method 
+// const gamename = new String('hardik-p');
+// const newString = gamename.substring(0,4)
+// console.log(newString);
+
+
+
+//          slice string method
+
+// const gamename = new String('hardik-p');
+
+// const anotherString=gamename.slice(-10,6)
+// console.log(anotherString);
+
+    // trim string method 
+
+// const newstringone = "     hitesh    ";
+// console.log(newstringone);
+// console.log(newstringone.trim());                       <=== trim() spaces ne dur kare chhe
+
+                                        //   replace() and searching 
+
+
+// const url="https://hardik.com/hardik%20panchal"; string method
+
+// console.log(url.replace('%20','-'))                      <=== it is used to replace the string
+// console.log(url.includes('hitesh'));                    <=== it is used to searching the string 
+
+
+                                //  split string method 
+
+// const gamename = new String('hardik-p-krishna');
+
+//  const anotherString=gamename.split('-')
+//  console.log(anotherString);
+
+
+
+
+
