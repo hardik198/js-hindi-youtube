e75c29d564cf2d1a2c25efd60942b1a19f6922f4
hardik198 <panchalhardik183@gmail.com>
2024-08-07 01:46:28 +0530

    Add files via upload

---
+++ 04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 < 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1)
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+
+// console.log(null >= 0); ans =true
+
+// console.log(undefined==0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);  <===ans=false
+
+
+// ==== 
+
+// console.log("2" === 2)  <===ans=false
+
+
