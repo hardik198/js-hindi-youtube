cd7d0ad7da7b6dbe27c83b4c5f65345daeb28a0a
hardik198 <panchalhardik183@gmail.com>
2024-08-08 02:19:23 +0530

    Add files via upload

---
+++ nums_and_math.js
@@ -0,0 +1,45 @@
+// const score = 400
+// console.log(score);
+
+//number define karvani second method
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());  <=== number ne string ma convert karva ni method
+
+// console.log(balance.toFixed(2));      <=== number ni pachal ketla 0 leva aena mate tofixed() method no use thay chhe
+
+// const othernumber = 123.4567
+
+// console.log(othernumber.toPrecision(4));  <=== number game atlo moto hoy but aene je size no number banavo hoy aena mate toPrecision() method no use thay he
+
+// const hunderds = 1000000;
+// console.log(hunderds.toLocaleString('en-IN'));  <=== jyare koi number moto hoy and tene comma lagai ne easily know karvo hoy to toLocaleString(en-IN) thi tamne indian format ma number easily know thai sakse 
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++ Math Functions in JS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));    <=== absolute value mate use thase
+// console.log(Math.round(4.699));   <==== rounding mate use thase
+// console.log(Math.ceil(4.2));          <==== rounding ma + value answer ma aavse
+// console.log(Math.floor(4.2));         <==== rounding ma - value answer aavse
+
+// console.log(Math.min(5,4,3,2,1));       <=== minimum value jova mate
+// console.log(Math.max(5,4,3,2,1));        <=== maximum value jova mate
+
+// console.log((Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random()* (max-min + 1)) + min);  <=== anwer range ni andar lava mate aa method use thase
+
+
+
+
+
+
+
+
