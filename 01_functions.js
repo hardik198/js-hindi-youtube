39d9e0c0688ed88e4a966b51f2420b032157f330
hardik198 <panchalhardik183@gmail.com>
2024-08-23 10:50:43 +0530

    Add files via upload

---
+++ 01_functions.js
@@ -0,0 +1,85 @@
+
+
+function sayMyName()
+{
+console.log("H");
+console.log("a");
+console.log("r");
+console.log("d");
+console.log("i");
+console.log("k");
+}
+// sayMyName();
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+
+// }
+
+
+
+
+// function addTwoNumbers(number1,number2){
+// //    let result=number1 + number2;
+// //    return result;
+//  return number1 + number2
+// }
+//  const result = addTwoNumbers(3,5);
+//  console.log(result);
+ 
+ 
+
+
+
+//  function loginUserMessage(username="sam"){
+//     if(username===undefined){
+//     console.log("please enter username");
+//      return
+//      }
+//       return `${username} just logged in`
+//   }
+//  console.log(loginUserMessage("hardik"));
+
+
+
+
+// function calculateCarePrice(val1,...num1){
+//     return num1;
+// }
+// console.log(calculateCarePrice(200,400,500));
+
+
+
+// const user={
+//     username:"hardik",
+//     price:199
+// }
+
+
+// function handleobject(anyobject)
+// {
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+// }
+
+
+
+// handleobject(user);
+
+// handleobject(
+//     {
+//         username:"hardik",
+//         price:199
+//     }
+// )
+
+
+
+
+// const myNewArray=[200,400,500,600]
+// function returnSecondValue(getarray)
+// {
+//     return getarray[2];
+// }
+// console.log(returnSecondValue(myNewArray));
+
