34a66f8512917ebfbc43a386f0eea1f6705f51c6
hardik198 <panchalhardik183@gmail.com>
2024-08-23 10:52:24 +0530

    Add files via upload

---
+++ 02_array-function.js
@@ -0,0 +1,31 @@
+const marvel_heros=["thor","Ironman","spiderman"];
+const dc_heros=["superman","flash","batman"];
+
+// marvel_heros.push(dc_heros);
+//  console.log(marvel_heros[3][0]);                  <================== push () method  ======================================================
+
+
+
+//   const all_heros= marvel_heros.concat(dc_heros);
+// console.log(all_heros);                           <==================  concat() method ===============================================
+
+
+
+//  const all_new_heros = [...marvel_heros,...dc_heros];
+//  console.log(all_new_heros);                              <================== spread() method =====================================
+
+
+
+// const another_array=[10,20,30,[40,50,60],70,[60,70,[4,5]]];
+// const real_another_array= another_array.flat(Infinity)
+// console.log(real_another_array);      <========= flat() method no use  many diffrent type na element ne bhega kari ne object ma display kare 6
+
+// console.log(Array.isArray("hardik"));     <============== aano use array 6 ke nai ae check karva mate thay 6 result true or false male 6
+// console.log(Array.from("hardik"));          <==================================== aano use array ma convert karva mate thay 6
+// console.log(Array.from({ name : "hardik"}));  //    interesting 
+
+// let score1=100
+// let score2=200
+// let score3=300
+// console.log(Array.of(score1,score2,score3));   <========== Array.of() no use different variable na element ne single array ma concat karva thay 6
+
