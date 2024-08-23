2a9bb15e76ebcad94ae205b1442699b0314a66f0
hardik198 <panchalhardik183@gmail.com>
2024-08-23 10:51:58 +0530

    Add files via upload

---
+++ 01_arrays.js
@@ -0,0 +1,56 @@
+// array 
+
+// const array=[10,20,30,40,50];
+// console.log(array[3]);
+
+// const myarr= new Array(10,20,30,40,50,60,70);
+// console.log(myarr[3]);
+
+// const arr1=[10,20,30,40,50]
+// arr1.push(60);
+// console.log(arr1);                       <============ push methode no use array ma last element ne add karva mate thay 6 ==================
+
+
+
+// const arr2=[10,20,30,40,50,60]
+//  arr2.pop();
+// console.log(arr2);       //    <============ pop method no use array mathi last element ne remove karva mate thay 6 ==================
+
+// const arr3=[10,20,30,40,50]
+// arr3.unshift(60);
+//  console.log(arr3);       // <============ unshift method no use array ma  element ne  starting ma add karva mate thay 6 ==================
+
+
+// const arr4=[10,20,30,40,50]
+//  arr4.shift();
+//  console.log(arr4);       //   <============ shift() method no use array mathi starting na element ne remove karva mate thay 6 ==================
+
+
+// const arr1=[10,20,30,40,50]
+// console.log(arr1.includes(50));  <========= includes() method no use array ma element 6 ke nai tene check karva mate thay 6 jo element array ma present hase to true result aavse and array ma element absent hase to false result aavse
+
+
+// const arr5=[10,20,30,40,50];
+// console.log(arr5.indexOf(50));      <======== indexof() method no use array ma element ni index knowing mate thay 6   =======================>
+
+
+// const myarr=[10,20,30,40,50];
+// const newarr=myarr.join()
+// console.log(myarr);
+//  console.log( typeof newarr);              <==== join() method no use array na element ne integer mathi string ma convert thay  6      ==========>   
+
+
+// const myarr=[10,20,30,40,50];
+// console.log("A",myarr);
+// const myn1=myarr.slice(0,5);           <==== slice() method no use array ma elemern ketla thi ketla sudhi element ne print karva ae mate thay 6 =====>
+// console.log(myn1);
+
+
+// const myarr=[10,20,30,40,50];
+// console.log("A",myarr);
+// const myn1=myarr.splice(1,4);
+// console.log(myn1);
+
+
+
+
