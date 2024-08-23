26cd2b4d46104b79f8372c887a7b25c3aa139860
hardik198 <panchalhardik183@gmail.com>
2024-08-23 10:51:01 +0530

    Add files via upload

---
+++ 02_scope.js
@@ -0,0 +1,33 @@
+
+//===================================================>  Scope In JavaScript  <================================================================
+
+
+// In js the let element baar declared karyo hoy and ae j element if condition ma declared karyo hoy to output ma condition ni andar value aavse 
+// In js the var element baar declared karyo hoy and ae j element if condition ma declared karyo hoy to output ma condition ni andar value aavse
+// In js the const element baar declared karyo hoy and ae j element if condition ma declared karyo hoy to output ma error aavse
+
+
+// var a=300;
+
+// if(true)
+// {
+//  a=10;
+//  b=20;
+//  c=30
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username="hardik"
+ function
+}
+
+
+
+
+
+
+
