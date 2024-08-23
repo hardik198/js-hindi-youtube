4db6547dace67babcb077a2e2ad3fbd9a48fe298
hardik198 <panchalhardik183@gmail.com>
2024-08-23 10:52:59 +0530

    Add files via upload

---
+++ 04_objects.js
@@ -0,0 +1,69 @@
+
+//     const tinderUser=new Object()
+// //  console.log(tinderUser);
+//  tinderUser.id="123abc";
+//  tinderUser.name="hardik";
+//  tinderUser.isloggIn=false
+// console.log(tinderUser);
+
+// const regularUser={
+//     email:"hardik123@gmail.com",
+//     fullname:{
+//                userfullname :{
+//                 firstname:"hardik",
+//                 lastname:"panchal"
+//                }
+//     }
+// }
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1={1:"a",2:"b"};
+// const obj2={3:"c",4:"d"};
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2)
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+// const users=[
+//     {
+//     id:1,
+//     email:"hardik123@gmail.com",
+//     }
+// ]
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));       <====== keys() method no use objets ni keys knowing mate thay 6
+// console.log(Object.values(tinderUser));   <======== values() method no use objects na elements ni value knowing mate thay 6
+// console.log(Object.entries(tinderUser));  <======= entries() method no use particular objects ne array ma convert karva mate thay 6=============
+
+// console.log(tinderUser.hasOwnProperty('isloggIn'));  <=== hasOwnProperty() method no use objects ni property chhe ke nai te chakashva mate thay 6
+
+
+//   ========================================> aa no use objects na elements ne display karva mate thay 6 <========================================
+// const cource={
+//     courcename:"js in hindi",
+//     price:"999",
+//     courceInstructer:"hitesh choudhry"
+// }
+// // cource.courceInstructer
+
+// const{courceInstructer:instructer}=cource;
+// console.log(instructer);
+
+
+
+
+// {
+//     "name":"hardik",
+//     "courcename" : "js in hindi",
+//     "price":"free",
+
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
